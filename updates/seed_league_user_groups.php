ea278081da9b74f5cf2e604f6e28638eae1e82e8
Paul Lovato <plovato@gmail.com>
2017-08-26 21:57:19 -0600

    Make the initial user ranks neutrally named.

--- updates/seed_league_user_groups.php
+++ updates/seed_league_user_groups.php
@@ -8,15 +8,15 @@ class SeedLeagueUserGroups extends Seeder
     public function run()
     {
         UserGroup::firstOrCreate([
-            'name' => 'Aether League Admin',
-            'code' => 'aether-admin',
-            'description' => 'Administrator for the Aether League.'
+            'name' => 'League Admin',
+            'code' => 'league-admin',
+            'description' => 'Administrator for the League.'
         ]);
 
         UserGroup::firstOrCreate([
-            'name' => 'Aether League Staff',
-            'code' => 'aether-staff',
-            'description' => 'Staff for the Aether League.'
+            'name' => 'League Staff',
+            'code' => 'league-staff',
+            'description' => 'Staff for the League.'
         ]);
     }
 }
