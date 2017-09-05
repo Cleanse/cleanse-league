14c34c3e86e66157905ffc4461788815386fbef1
Paul Lovato <plovato@gmail.com>
2017-09-04 20:23:41 -0600

    Refactored season management component.

--- components/ManagerSeason.php
+++ components/ManagerSeason.php
@@ -67,7 +67,7 @@ class ManagerSeason extends ComponentBase
 
     protected function modeCreate()
     {
-        $check = Season::whereNull('finished_at')->first();
+        $check = Season::whereNull('winner_id')->first();
 
         if ($check) {
             Flash::error('Season: "' . $check->name . '" has not completed yet.');
@@ -86,6 +86,24 @@ class ManagerSeason extends ComponentBase
         $this->page['championships'] = $championships;
     }
 
+    public function onCreateSeason()
+    {
+        $cId = post('championship');
+        $seasonName = post('name');
+
+        $championship = Championship::find($cId);
+
+        $season = $championship->seasons()->create([
+            'name' => $seasonName
+        ]);
+
+        Flash::success('Season: "' . $season->name . '" was created.');
+
+        Session::flash('flashSuccess', true);
+
+        return Redirect::refresh();
+    }
+
     protected function modeEdit()
     {
         $this->model = post('season_id');
@@ -93,9 +111,24 @@ class ManagerSeason extends ComponentBase
         $this->page['season'] = Season::find($this->model);
     }
 
+    public function onEditSeason()
+    {
+        $editSeason = Season::find(post('season_id'));
+
+        $editSeason->name = post('name');
+
+        $editSeason->save();
+
+        Flash::success('Season ' . $editSeason->name . ' was edited.');
+
+        Session::flash('flashSuccess', true);
+
+        return Redirect::refresh();
+    }
+
     protected function modeTeams()
     {
-        $check = Season::whereNull('finished_at')
+        $check = Season::whereNull('winner_id')
             ->has('matches')
             ->first();
 
@@ -107,7 +140,7 @@ class ManagerSeason extends ComponentBase
             return Redirect::refresh();
         }
 
-        $season = Season::with('teams.team')->whereNull('finished_at')->first();
+        $season = Season::with('teams.team')->whereNull('winner_id')->first();
 
         $seasonTeams = DB::table('cleanse_league_event_teams')
             ->join('cleanse_league_teams', 'cleanse_league_event_teams.team_id', '=', 'cleanse_league_teams.id')
@@ -129,59 +162,60 @@ class ManagerSeason extends ComponentBase
         $this->page['season'] = $season->id;
     }
 
-    protected function modeSchedule()
-    {
-        $check = Season::whereNull('finished_at')
-            ->has('matches')
-            ->first();
-
-        if ($check) {
-            Flash::error('Season: "' . $check->name . '" has a schedule already.');
-
-            Session::flash('flashSuccess', true);
-
-            return Redirect::refresh();
-        }
-
-        $this->page['season'] = Season::whereNull('finished_at')->get();
-    }
-
-    public function onCreateSeason()
-    {
-        $cId = post('championship');
-        $seasonName = post('name');
-
-        $championship = Championship::find($cId);
-
-        $season = $championship->seasons()->create([
-            'name' => $seasonName
-        ]);
-
-        Flash::success('Season: "' . $season->name . '" was created.');
-
-        Session::flash('flashSuccess', true);
-
-        return Redirect::refresh();
-    }
-
-    public function onEditSeason()
-    {
-        $editSeason = Season::find(post('season_id'));
-
-        $editSeason->name = post('name');
-
-        $editSeason->save();
-
-        Flash::success('Season ' . $editSeason->name . ' was edited.');
-
-        Session::flash('flashSuccess', true);
-
-        return Redirect::refresh();
-    }
-
     public function onAddTeam()
     {
         $team_id = post('team_id');
+        $season_id = post('season_id');
+
+        $season = Season::with('teams')->find($season_id);
+        $season->teams()->create([
+            'team_id' => $team_id,
+        ]);
+
+        $seasonTeams = DB::table('cleanse_league_event_teams')
+            ->join('cleanse_league_teams', 'cleanse_league_event_teams.team_id', '=', 'cleanse_league_teams.id')
+            ->select('cleanse_league_event_teams.id', 'cleanse_league_event_teams.team_id', 'cleanse_league_teams.name')
+            ->where('cleanse_league_event_teams.teamable_type', 'season')
+            ->where('cleanse_league_event_teams.teamable_id', $season_id)
+            ->orderBy('cleanse_league_teams.name', 'asc')
+            ->get();
+
+        $idVals = [];
+        foreach ($seasonTeams as $team) {
+            $idVals[] = $team->team_id;
+        }
+
+        $teams = Team::whereNotIn('id', $idVals)->orderBy('name', 'asc')->get();
+
+        $this->page['mode'] = 'teams';
+        $this->page['season'] = $season_id;
+        $this->page['teams'] = $teams;
+        $this->page['eteams'] = $seasonTeams;
+    }
+
+    public function getCreateTeamFormAttributes()
+    {
+        $attributes = [];
+
+        $attributes['method'] = 'POST';
+        $attributes['data-request'] = $this->alias . '::onCreateTeam';
+        $attributes['data-request-update'] = "'" . $this->alias . "::teams':'#editor-area'";
+
+        return $attributes;
+    }
+
+    public function onCreateTeam()
+    {
+        $newTeam = new Team;
+        $newTeam->name = post('name');
+
+        if (post('initials') !== '') {
+            $newTeam->initials = post('initials');
+        }
+
+        $newTeam->save();
+
+        $team_id = $newTeam->id;
         $season_id = post('season_id');
 
         $season = Season::with('teams')->find($season_id);
@@ -236,6 +270,67 @@ class ManagerSeason extends ComponentBase
         $this->page['season'] = $season_id;
         $this->page['teams'] = $teams;
         $this->page['eteams'] = $seasonTeams;
+    }
+
+    public function getSearchFormAttributes()
+    {
+        $attributes = [];
+
+        $attributes['method'] = 'POST';
+        $attributes['data-request'] = $this->alias . '::onSearchForTeam';
+        $attributes['data-request-update'] = "'" . $this->alias . "::search-results':'#team-list'";
+        $attributes['class'] = 'justify-content-end';
+
+        return $attributes;
+    }
+
+    public function onSearchForTeam()
+    {
+        $search = post('search');
+
+        if (!$search) {
+            throw new ValidationException(['name' => 'You must enter at least part of a team\'s name!']);
+        }
+
+        $season = Season::with('teams.team')->whereNull('winner_id')->first();
+
+        $seasonTeams = DB::table('cleanse_league_event_teams')
+            ->join('cleanse_league_teams', 'cleanse_league_event_teams.team_id', '=', 'cleanse_league_teams.id')
+            ->select('cleanse_league_event_teams.id', 'cleanse_league_event_teams.team_id', 'cleanse_league_teams.name')
+            ->where('cleanse_league_event_teams.teamable_type', 'season')
+            ->where('cleanse_league_event_teams.teamable_id', $season->id)
+            ->orderBy('cleanse_league_teams.name', 'asc')
+            ->get();
+
+        $idVals = [];
+        foreach ($seasonTeams as $team) {
+            $idVals[] = $team->team_id;
+        }
+
+        $list = Team::where('name', 'like', '%' . $search . '%')
+            ->whereNotIn('id', $idVals)
+            ->orderBy('name', 'asc')
+            ->get();
+
+        $this->page['items'] = $list;
+        $this->page['season'] = post('season');
+    }
+
+    protected function modeSchedule()
+    {
+        $check = Season::whereNull('finished_at')
+            ->has('matches')
+            ->first();
+
+        if ($check) {
+            Flash::error('Season: "' . $check->name . '" has a schedule already.');
+
+            Session::flash('flashSuccess', true);
+
+            return Redirect::refresh();
+        }
+
+        $this->page['season'] = Season::whereNull('finished_at')->get();
     }
 
     public function onCreateSchedule()
